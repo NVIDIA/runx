9c9fb9fd22f106aa58b32c53485eb8ecd8ef621d
Andrew Tao <atao@nvidia.com>
2020-04-02 08:49:44 -0700

    updated revision

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 parent = Path(__file__).resolve().parent
 setup(
     name="runx",
-    version="0.0.3",
+    version="0.0.4",
     author="Andrew Tao",
     author_email="atao@nvidia.com",
     description="runx - experiment manager for machine learning research",
